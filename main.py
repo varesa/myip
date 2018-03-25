a2d79b889b8c249580ed3dced9dd27c60f0dc429
Esa Varemo <esa@kuivanto.fi>
2018-03-25 22:32:23 +0300

    Get the IP from proxy header

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def showmyip():
-    ip = request.remote_addr
-    return str(ip)
+    if 'X-Forwarded-For' in request.headers.keys():
+        source_ip = request.headers['X-Forwarded-For']
+        return str(source_ip)
+    else:
+        source_ip = request.remote_addr
+        return "No proxy header found, result may be inaccurate:" + str(source_ip)
